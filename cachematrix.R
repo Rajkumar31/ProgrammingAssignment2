a2d43f17b720b9bd0b8018c8968b333389a99cb9
Rajkumar31 <rajkumarr.310593@gmail.com>
2014-12-18 14:06:32 +0530

    cachematrix.R
    Set and get matrix done

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The first function, makeVector creates a special "vector", which is really a list containing a function to
+## 1) Set the value of the vector
+## 2) Get the value of the vector
+## 3) Set the value of the mean
+## 4) Get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
+        # initially nothing is cached so set it to NULL and it is used to hold cache
+        cache <- NULL
+        
+        # store a matrix
+        setMatrix <- function(newValue) {
+        x <<- newValue
+        # since the matrix is assigned a new value, flush the cache
+        cache <<- NULL
+        }
 
+        # returns the stored matrix
+        getMatrix <- function() {
+        x
+        }
 }
 
 
